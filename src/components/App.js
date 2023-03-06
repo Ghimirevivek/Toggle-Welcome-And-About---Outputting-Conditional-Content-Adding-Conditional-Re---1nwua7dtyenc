dbadfc66d8ad01d0314329ac7411a7fa91405a96
Ghimirevivek <108508449+Ghimirevivek@users.noreply.github.com>
2023-03-06 15:26:44 +0530

    edited

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "../styles/App.css";
-
+import {useState} from "react"
 const App = () => {
+  const [welcome,setwelcome] = useState(true)
+  const handler = ()=>{
+    setwelcome((prev)=>!prev)
+  }
   return (
     <div id="main">
-      <div id="welcome-div">
+    {welcome? (<div id="welcome-div">
         <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
+      </div>) : (<div id="about-div">
         <h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.
         </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+      </div>)}
+      <button onClick={handler} id="toggle">Toggle</button>
     </div>
   );
 };
